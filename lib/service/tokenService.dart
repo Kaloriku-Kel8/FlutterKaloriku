d5d6ae1559cc017abb6be2c434e096c2e2e5a6f9
paul-awan <pwink170@gmail.com>
2024-11-21 09:43:18 +0700

    add : resepMakananController

--- lib/service/tokenService.dart
+++ lib/service/tokenService.dart
@@ -2,11 +2,12 @@ import 'package:kaloriku/service/authService.dart';
 
 class ApiService {
   final AuthService _authService = AuthService();
-  Future<Map<String, String>> _getHeaders() async {
+  Future<Map<String, String>> getHeaders() async {
     final token = await _authService.getToken();
     return {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer $token',
+      'Accept': 'application/json',
     };
   }
 }
